5a6ed3354c1a0168d8a6a4a1a7c144f1753b8cb8
KailinTang1995 <KailinTang1995@gmail.com>
2018-02-01 00:45:17 -0500

    Search Bar Done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import {ShotChart} from './ShotChart';
+import nba from 'nba';
 import {Profile} from './Profile';
 import {DataViewContainer} from './DataViewContainer';
-
-import nba from 'nba';
+import {SearchBar} from './SearchBar'
 
 export class Main extends React.Component {
     state = {
-        playerId: nba.findPlayer('Stephen Curry').playerId,
-        playerInfo: {}
+        playerInfo: {},
+    }
+
+    handleSelectPlayer = (name) => {
+        this.loadPlayerInfo(name);
     }
 
     componentDidMount() {
-        nba.stats.playerInfo({PlayerID: this.state.playerId}).then((info) => {
+        this.loadPlayerInfo('Stephen Curry');
+    }
+
+    loadPlayerInfo = (playerName) => {
+        const id = nba.findPlayer(playerName).playerId;
+        nba.stats.playerInfo({PlayerID: id}).then((info) => {
             const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
             console.log(playerInfo);
             this.setState({
-                playerInfo: playerInfo,
+                playerInfo: playerInfo
             });
         });
     }
@@ -24,8 +31,11 @@ export class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <Profile playerId={this.state.playerId} playerInfo={this.state.playerInfo}/>
-                <DataViewContainer playerId={this.state.playerInfo.playerId}/>
+                <SearchBar handleSelectPlayer={this.handleSelectPlayer}/>
+                <div className="player">
+                    <Profile playerInfo={this.state.playerInfo}/>
+                    <DataViewContainer playerId={this.state.playerInfo.playerId}/>
+                </div>
             </div>
         );
     }
